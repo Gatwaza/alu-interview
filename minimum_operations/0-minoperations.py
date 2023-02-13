496c61d75399115e573692d52dc5274f7ea1a612
Gatwaza <gatwazarobert177@gmail.com>
2023-02-13 02:01:07 -0800

    n of ops

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-
 """
+Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
 
-This module allows pasting of contents 
-exceeding target number.
-
+Prototype: def minOperations(n)
+Returns an integer
+If n is impossible to achieve, return 0
 """
 
 def minOperations(n):
-    if n <= 0:
-        return 0
-    result = 0
-    i = 2
-    while i * i <= n:
-        while n % i == 0:
-            result += 1
-            n = n // i
-        i += 1
-    if n > 1:
-        result += 1
-    return result
-
+    a = 0
+    b = 2
+    while n > 1:
+        while n % b == 0:
+            a += b
+            n = n / b
+        b += 1
+    return a
