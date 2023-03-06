0be7cae8dec8631eea15e5c9532396fe0162873a
Gatwaza <gatwazarobert177@gmail.com>
2023-03-06 03:09:24 -0800

    couples of operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-Given a number n, write a method that calculates the fewest # of operations
+Given a number n, write a method that calculates the fewest # of operations.
+
 Prototype: def minOperations(n)
 Returns an integer
 If n is impossible to achieve, return 0
 """
 
   def minOperations(n):
-    a = 0
-    b = 2
-    while n > 1:
-        while n % b == 0:
-            a += b
-            n = n / b
-        b += 1
-    return a
+     a = 0
+     b = 2
+     while n > 1:
+         while n % b == 0:
+             a += b
+             n = n / b
+         b += 1
+     return a
