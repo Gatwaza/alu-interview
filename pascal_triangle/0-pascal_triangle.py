ddd0237b891b42e591823ea1071b3339a3d91a4c
Gatwaza <gatwazarobert177@gmail.com>
2023-03-13 03:47:08 -0700

    achieved triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -21,7 +21,7 @@ def pascal_triangle(n):
     for i in range(2, n):
         row = [1]
         for j in range(1, i):
-            element = triangle[i-1][j-1] + triangle[i-1][j]
+            element = triangle[i - 1][j - 1] + triangle[i - 1][j]
             row.append(element)
         row.append(1)
         triangle.append(row)
