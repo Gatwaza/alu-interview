927697d527a544408c3a9a4dd1199c5be87ba136
Gatwaza <gatwazarobert177@gmail.com>
2023-03-13 02:48:00 -0700

    achieved triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# Return empty list if n is less than or equal to 0
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1], [1, 1]]
+
+    if n <= 2:
+        return triangle[:n]
+
+    for i in range(2, n):
+        row = [1]
+        for j in range(1, i):
+            element = triangle[i-1][j-1] + triangle[i-1][j]
+            row.append(element)
+        row.append(1)
+        triangle.append(row)
+
+    return triangle    
